c863a49d4641ef7af78c2ba9fd3efca46e74b85a
djumanov <diyorbekjumanov1120@gmail.com>
2022-11-23 17:58:03 +0500

    added some func

--- user.py
+++ user.py
@@ -8,3 +8,39 @@ def get_users_by_country(data: dict, country: str) -> list[dict]:
     Returns: 
         list: list of users. item ex: {'full_name': f'{first} {last} {title}', 'age': int}
     '''
+
+
+def get_users_by_age(data: dict, age: int) -> list[dict]:
+    '''get users by age
+    
+    Args:
+        data (dict): users data
+        age (int): age
+
+    Returns: 
+        list: list of users. item ex: {'full_name': f'{first} {last} {title}', 'age': int}
+    '''
+
+
+def get_users_by_city(data: dict, city: str) -> list[dict]:
+    '''get users by city
+    
+    Args:
+        data (dict): users data
+        city (str): city name
+
+    Returns: 
+        list: list of users. item ex: {'full_name': f'{first} {last} {title}', 'age': int}
+    '''
+
+
+def get_users_by_nat(data: dict, nat: str) -> list[dict]:
+    '''get users by nat
+    
+    Args:
+        data (dict): users data
+        nat (str): user nat
+
+    Returns: 
+        list: list of users. item ex: {'full_name': f'{first} {last} {title}', 'age': int}
+    '''
